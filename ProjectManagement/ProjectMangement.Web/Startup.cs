cc55732140f6e1477faa1ea4f766fea6de1e7a44
Hamid Barani <hamid.vs.life@gmail.com>
2021-02-12 14:35:39 +0330

    Add components to web layers

--- ProjectManagement/ProjectMangement.Web/Startup.cs
+++ ProjectManagement/ProjectMangement.Web/Startup.cs
@@ -16,6 +16,8 @@ namespace ProjectMangement.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,20 @@ namespace ProjectMangement.Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
+
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
